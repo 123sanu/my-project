7203008f057e14715e2d872ecce0a297e07a7988
123sanu <sanucseduet@gmail.com>
2018-12-02 23:43:55 +0600

    We set a name for funtion greeting

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
 public class Hello{
     public static void main(String[] args){
         System.out.println("Laravel is the best PHP Framework..");
+         greeting("Laravel 5.7");
+    }
+
+    public static void greeting(String name){
+      System.out.println("Welcome To " + name);
     }
 }
