f8b6077fc4f7b9cb26075a090648244b816bec3a
123sanu <sanucseduet@gmail.com>
2018-12-03 00:19:58 +0600

    Now we add new method sqrt in branchB

--- Hello.java
+++ Hello.java
@@ -2,9 +2,14 @@ public class Hello{
     public static void main(String[] args){
         System.out.println("CodeIgniter is the easy framework ..");
          view("CodeIgniter Version 3.1.3");
+         sqrt(5);
     }
 
     public static void view(String name){
       System.out.println("Welcome To " + name);
+    }
+
+    public static sqrt(int x){
+        return x*x;
     }
 }
